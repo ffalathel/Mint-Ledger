f48164fad51b53e001d9f0a3a10a836a31208990
ffalathel <fahdahalathel@gmail.com>
2024-09-22 18:54:12 -0400

    link to sign up from get started1

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,8 +1,14 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import { useUser, UserButton } from "@clerk/nextjs"
+import Link from 'next/link'
+
 
 function Header() {
+
+  const {user,isSignedIn}= useUser();
   return (
     <div className='p-5 flex justify-between items-center border shadow-md'>
         <Image src={'./logo.svg'}
@@ -10,7 +16,14 @@ function Header() {
         width={100}
         height={100}
         />
-      <Button>Get Started</Button>
+ 
+      {isSignedIn?
+      <UserButton/> : 
+      <Link href = {'/sign-in'}> 
+        <Button>Get Started</Button>
+      </Link>
+      }
+
        
     </div>
   )
