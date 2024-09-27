3ffc846038908198cb08eb45f1eecc1e24f29bb0
ffalathel <fahdahalathel@gmail.com>
2024-09-27 12:27:00 -0400

    "Updated drizzle.config.js, package-lock.json, and package.json files; added pg dependency; modified utils/dbConfig.jsx and utils/schema.jsx files"

--- utils/schema.jsx
+++ utils/schema.jsx
@@ -1,9 +1,9 @@
-import { pgTable } from require("drizzle-orm/pg-core");
+import { pgTable, serial, varchar} from 'drizzle-orm/pg-core';
 
 export const Budgets=pgTable('budget',{
    id:serial('id').primaryKey(),
    name:varchar('name').notNull(),
    amount:varchar('amount').notNull(),
    icon:varchar('icon'),
-   createdBy:varchar("createdBy").notNull()
+   createdBy:varchar('createdBy').notNull()
 })
