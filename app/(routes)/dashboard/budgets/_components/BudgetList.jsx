e504a1c30210c88a4014190270082a98174d9dc7
ffalathel <fahdahalathel@gmail.com>
2024-10-09 16:17:53 -0400

    "Updated BudgetList component to fetch and display budget list with total spend and items, and added BudgetItem component for each budget"

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,30 +1,38 @@
+"use client"
 import { db } from '@/utils/dbConfig'
-import { Budgets } from '@/utils/schema'
+import { Budgets, Expenses } from '@/utils/schema'
 import { useUser } from '@clerk/nextjs'
-import { getTableColumns } from 'drizzle-orm'
+import {eq, getTableColumns, sql } from 'drizzle-orm'
 import React, { useEffect } from 'react'
+import { useState } from 'react'
+import BudgetItem from './BudgetItem'
 import CreateBudget from './CreateBudget'
 
 function BudgetList() {
+  const [budgetList, setBudgetList]=useState([]);
+  const {user}=useUser();
   useEffect(()=>{
     user&&getBudgetList();
   },[user])
   const getBudgetList= async()=>{
-    const {user}=useUser();
     const result=await db.select({
       ...getTableColumns(Budgets),
-      totalSpend: sql `sum(${Epenses.amount})`.mapWith(Number),
-      totalItem: sql `sum(${Epenses.id})`.mapWith(Number)
+      totalSpend: sql `sum(${Expenses.amount})`.mapWith(Number),
+      totalItem: sql `sum(${Expenses.id})`.mapWith(Number)
     }).from(Budgets)
-    .leftjoin(Expenses,eq(Budgets.id,Expenses.budgetid))
+    .leftJoin(Expenses,eq(Budgets.id,Expenses.budgetid))
     .where(eq(Budgets.createdBy,user.primaryEmailAddress?.emailAddress))
     .groupBy(Budgets.id)
-    console.log(result);
+    setBudgetList(result);
+
   }
   return (
     <div className='mt-7'>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'> 
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'> 
             <CreateBudget/>
+            {budgetList.map((budget,index)=>(
+              <BudgetItem budget={budget}/>
+            ))}
         </div>
     </div>
   )
