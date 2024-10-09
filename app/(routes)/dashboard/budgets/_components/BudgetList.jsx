175fed9c18764a1f2bc405b7a61fc8170ac2c07a
ffalathel <fahdahalathel@gmail.com>
2024-10-09 15:33:57 -0400

    "Added user authentication check to BudgetList component and refactored getBudgetList function"

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,20 +1,25 @@
 import { db } from '@/utils/dbConfig'
 import { Budgets } from '@/utils/schema'
+import { useUser } from '@clerk/nextjs'
 import { getTableColumns } from 'drizzle-orm'
-import React from 'react'
+import React, { useEffect } from 'react'
 import CreateBudget from './CreateBudget'
 
 function BudgetList() {
-
+  useEffect(()=>{
+    user&&getBudgetList();
+  },[user])
   const getBudgetList= async()=>{
-    
+    const {user}=useUser();
     const result=await db.select({
       ...getTableColumns(Budgets),
       totalSpend: sql `sum(${Epenses.amount})`.mapWith(Number),
       totalItem: sql `sum(${Epenses.id})`.mapWith(Number)
     }).from(Budgets)
     .leftjoin(Expenses,eq(Budgets.id,Expenses.budgetid))
+    .where(eq(Budgets.createdBy,user.primaryEmailAddress?.emailAddress))
     .groupBy(Budgets.id)
+    console.log(result);
   }
   return (
     <div className='mt-7'>
