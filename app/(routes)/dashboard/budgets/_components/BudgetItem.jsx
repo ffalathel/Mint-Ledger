ee036bd1ebf9a6cd561f8d408ed6f16041fb01ad
ffalathel <fahdahalathel@gmail.com>
2024-10-11 15:05:01 -0400

    "Added optional chaining to BudgetItem component and integrated it into Expense page"

--- app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
@@ -9,17 +9,17 @@ function BudgetItem({budget}) {
         <div className='flex gap-2 items-center'>
             <h2 className='tex-2xl p-3 bg-slate-100 rounded-full px-4'>{budget?.icon}</h2> 
             <div>
-                <h2 className='font-bold'>{budget.name}</h2>
-                <h2 className='text-sm text-gray-500'>{budget.totalItem?budget.totalItem:0} Item</h2>
+                <h2 className='font-bold'>{budget?.name}</h2>
+                <h2 className='text-sm text-gray-500'>{budget?.totalItem?budget.totalItem:0} Item</h2>
             </div>
        </div>
-       <h2 className='font-bold text-primary text-lg'>${budget.amount}</h2>     
+       <h2 className='font-bold text-primary text-lg'>${budget?.amount}</h2>     
     </div>
 
         <div className='mt-5'>
             <div className='flex items-center justify-between mb-3'>
-               <h2 className='text-xs text-slate-400'>${budget.totalSpend?budget.totalSpend:0} Spent</h2>
-               <h2 className='text-xs text-slate-400'>${budget.amount-budget.totalSpend} Remaining</h2>  
+               <h2 className='text-xs text-slate-400'>${budget?.totalSpend?budget?.totalSpend:0} Spent</h2>
+               <h2 className='text-xs text-slate-400'>${budget?.amount-budget?.totalSpend} Remaining</h2>  
             </div>
             <div className='w-full bg-slate-300 h-2 rounded-full'>
                 <div className='w-[40%] bg-primary h-2 rounded-full'>
--- app/(routes)/dashboard/expenses/[id]/page.jsx
+++ app/(routes)/dashboard/expenses/[id]/page.jsx
@@ -3,13 +3,15 @@ import { db } from '@/utils/dbConfig'
 import { Budgets, Expenses } from '@/utils/schema'
 import { useUser } from '@clerk/nextjs'
 import { eq, getTableColumns, sql } from 'drizzle-orm'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import BudgetItem from '../../budgets/_components/BudgetItem'
 
 function Expense({params}) {
+  const [budgetInfo, setBudgetInfo]=useState();
   const {user}= useUser();
   useEffect(()=>{
     user&&getBudgetINfo();
-  },[user])
+  },[user]);
     const getBudgetINfo = async()=>{
         const result=await db.select({
             ...getTableColumns(Budgets),
@@ -20,12 +22,16 @@ function Expense({params}) {
           .where(eq(Budgets.createdBy,user.primaryEmailAddress?.emailAddress))
           .where(eq(Budgets.id,params.id))
           .groupBy(Budgets.id)
+          setBudgetInfo(result[0]);
           
-          console.log(result)
     }
   return (
     <div className='p-10'>
         <h2 className='text-2xl font-bold'>My Expenses</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 mt-6'>
+            <BudgetItem budget={budgetInfo}/>
+
+        </div>
     </div>
   )
 }
