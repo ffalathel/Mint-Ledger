3baf03f78d5a788f40cb4138a8a1e5b16cf53ed6
ffalathel <fahdahalathel@gmail.com>
2024-10-11 14:57:49 -0400

    "Wrap BudgetItem component with Next.js Link component and update CSS class"

--- app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 function BudgetItem({budget}) {
 
   return (
-    <div className='p-5 border rounded-lg hover:shadow-md cursor-pointer'>
+    <Link href={'/dashboard/expenses/'+budget?.id} className='p-5 border rounded-lg hover:shadow-md cursor-pointer'>
         <div className='flex gap-2 items-center justify-between'>
         <div className='flex gap-2 items-center'>
             <h2 className='tex-2xl p-3 bg-slate-100 rounded-full px-4'>{budget?.icon}</h2> 
@@ -26,7 +27,7 @@ function BudgetItem({budget}) {
                 </div>
             </div>
         </div>
-    </div>
+    </Link>
 
   )
 }
